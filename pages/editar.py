07d83dd1da21abd94dd89aaff533520ba17cbd8e
rafapatekoski <insightfulpate@gmail.com>
2024-01-08 12:31:34 -0300

    Edição funcionando e não transformando id em string

--- pages/editar.py
+++ pages/editar.py
@@ -1,32 +1,46 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+import pandas as pd
+from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
 import streamlit as st
-import pandas as pd
+
 load_dotenv(find_dotenv())
 password = os.environ.get("MONGODB_PWD")
 uri = f"mongodb+srv://patekoski:{password}@cluster0.isfvbxi.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    st.write("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    st.write(e)
 
-dbs = client.list_database_names()
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Access the database and collection
 teste_db = client.escola
-collections = teste_db.list_collection_names()
-aluno = collections[0]
-df = pd.read_csv("listapiloto2024.csv", sep=';')
+pandasteste = teste_db.pandasteste
+
+# Consulte todos os documentos na coleção
+colecaopandateste = pandasteste.find()
+
+# Transforme os documentos em um DataFrame do pandas
+df = pd.DataFrame(list(colecaopandateste))
+
+# Remova a coluna '_id' do DataFrame
+df = df.drop('_id', axis=1, errors='ignore')
+
+# Use o Streamlit para editar o DataFrame
 df_editado = st.data_editor(df, num_rows="dynamic")
 
+# Função para salvar as alterações de volta no MongoDB
 def editar():
-    collection = teste_db.pandasteste
-    st.write(collection)
+    # Obtenha os dados editados como uma lista de dicionários
     dados_json = df_editado.to_dict(orient='records')
-    collection.delete_many({})
-    collection.insert_many(dados_json)
+    
+    # Remova '_id' de cada documento, se presente
+    for doc in dados_json:
+        doc.pop('_id', None)
+    
+    # Delete all documents in the collection
+    pandasteste.delete_many({})
+
+    # Insert the edited documents without '_id' back into the collection
+    pandasteste.insert_many(dados_json)
+
+# Botão para chamar a função de edição
 st.button("Editar", on_click=editar)
